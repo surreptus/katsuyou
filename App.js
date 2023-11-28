6796166d879947d862a3fbcbdbaeac5053b39aaf
surreptus <47543507+surreptus@users.noreply.github.com>
2023-11-28 15:57:27 -0800

    feat: add basic screens

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ title, navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>this is the home screen: <Button title="Press Me" onPress={() => navigation.navigate("Detail")}/></Text>
+    </View>
+  );
+}
+
+function DetailScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'red', justifyContent: 'center' }}>
+      <Text>details detail details</Text>
+      <Text>this is the detail screen: <Button title="Press Me" onPress={() => navigation.navigate("Home")}/></Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} screenOptions={{title: "this is my title" }} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
