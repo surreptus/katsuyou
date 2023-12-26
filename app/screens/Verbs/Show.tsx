9c8c36fc508ee6e5499cb973bf4572935fff7bc1
surreptus <47543507+surreptus@users.noreply.github.com>
2023-12-25 19:34:30 -0600

    fix: add usage of slug

--- app/screens/Verbs/Show.tsx
+++ app/screens/Verbs/Show.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export default function Show({ navigation }) {
+export default function Show({ route, navigation }) {
+  const { slug } = route.params;
+
   return (
     <View>
       <Text>Verbs Show</Text>
+      <Text>{slug}</Text>
     </View>
   );
 }
