94f806f4a77311e32bbfc0f2642f9b9f22f7b53e
surreptus <47543507+surreptus@users.noreply.github.com>
2024-06-16 18:22:46 -0700

    chore: add different modes

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,19 +3,24 @@ import { Practice } from "./pages/Practice";
 import { Settings } from "./pages/Settings";
 import { Vocabulary } from "./pages/Vocabulary";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Practice />,
+    },
+    {
+      path: "/settings",
+      element: <Settings />,
+    },
+    {
+      path: "/vocabulary",
+      element: <Vocabulary />,
+    },
+  ],
   {
-    path: "/",
-    element: <Practice />,
-  },
-  {
-    path: "/settings",
-    element: <Settings />,
-  },
-  {
-    path: "/vocabulary",
-    element: <Vocabulary />,
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
 
 export const Router = () => RouterProvider({ router });
