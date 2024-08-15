d1b92227c7172d69e7a1f0258ec04febd902c79c
surreptus <47543507+surreptus@users.noreply.github.com>
2024-08-14 22:47:51 -0700

    test: add button test

---
+++ src/components/__tests__/Button.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/react";
+import { Button } from "../Button";
+
+describe("Button", () => {
+  it("should render the button", () => {
+    const screen = render(<Button>Click me</Button>);
+
+    expect(screen.getByRole("button")).toBeDefined();
+  });
+});
